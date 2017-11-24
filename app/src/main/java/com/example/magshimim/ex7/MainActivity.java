ea66c2630176a5a1ddd0f4ed553f2f65a4249d59
tomeren1 <31760835+tomeren1@users.noreply.github.com>
2017-11-24 11:03:15 +0200

    2

--- app/src/main/java/com/example/magshimim/ex7/MainActivity.java
+++ app/src/main/java/com/example/magshimim/ex7/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.magshimim.ex7;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends Activity {
 
@@ -10,4 +15,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+    /** Called when the user taps the Send button
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }*/
 }
