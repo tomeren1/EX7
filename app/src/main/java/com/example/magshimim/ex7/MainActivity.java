b83b79670bd9018cb492e1fa36cac901cf8324c5
tomeren1 <31760835+tomeren1@users.noreply.github.com>
2018-01-15 13:26:45 +0200

    2

--- app/src/main/java/com/example/magshimim/ex7/MainActivity.java
+++ app/src/main/java/com/example/magshimim/ex7/MainActivity.java
@@ -1,27 +1,39 @@
 package com.example.magshimim.ex7;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+    final int REGISTERCODE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage1(View v) {
+        Intent i=new Intent(this,RegisterActivity.class);
+        startActivityForResult(i,REGISTERCODE);
     }
 
-    /** Called when the user taps the Send button
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }*/
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Resources res =getResources();
+        String gender="";
+        TextView tv = (TextView) findViewById(R.id.tvIntro);
+        if (requestCode == REGISTERCODE && resultCode == RESULT_OK) {
+            if (data.getStringExtra("Gender").compareTo("Male") == 0) {
+                gender ="Mr.";
+            } else
+                gender = "Mrs.";
+        }
+        tv.setText(res.getString(R.string.Welcome ,gender,data.getStringExtra("First Name"),data.getStringExtra("Last Name")));
+        Button bt = (Button) findViewById(R.id.bt1);
+        bt.setText(R.string.buttonwelcome);
+    }
 }
+
--- app/src/main/java/com/example/magshimim/ex7/RegisterActivity.java
+++ app/src/main/java/com/example/magshimim/ex7/RegisterActivity.java
@@ -1,7 +1,13 @@
 package com.example.magshimim.ex7;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class RegisterActivity extends Activity {
 
@@ -9,5 +15,23 @@ public class RegisterActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+    }
+
+    public void sendMessage2(View v) {
+        Intent intent = new Intent (this, RegisterActivity.class);
+        EditText edt = (EditText)findViewById(R.id.input1);
+        String s1 = edt.getText().toString();
+        intent.putExtra("First Name", s1);
+        EditText edt2 = (EditText)findViewById(R.id.input2);
+        String s2 = edt2.getText().toString();
+        intent.putExtra("Last Name", s2);
+        RadioGroup rg = (RadioGroup)  findViewById(R.id.rg);
+        int gender = rg.getCheckedRadioButtonId();
+        RadioButton rb = (RadioButton)  findViewById(gender);
+        String s3 = rb.getText().toString();
+        intent.putExtra("Gender", s3);
+        setResult(RESULT_OK,intent);
+        finish();
     }
 }
